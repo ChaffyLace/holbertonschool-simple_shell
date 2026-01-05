dd672208135c9b624f69c0a4508227922fd0865e
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-05 20:16:02 +0100

    Task 2

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int ac, char **av)
 	char **args;
 	int count = 0;
 
-	(void)ac;
+	(void)ac; 
 	while (1)
 	{
 		count++;
@@ -22,24 +22,21 @@ int main(int ac, char **av)
 			write(STDOUT_FILENO, "$ ", 2);
 
 		read = getline(&line, &len, stdin);
+		
 		if (read == -1)
 		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
 			free(line);
 			break;
 		}
-
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
 		args = split_line(line);
+		
 		if (args[0] != NULL)
 		{
-			if (strcmp(args[0], "exit") == 0)
-			{
-				free_array(args);
-				free(line);
-				exit(0);
-			}
 			execute_command(args, line, av[0], count);
 		}
 		free_array(args);
