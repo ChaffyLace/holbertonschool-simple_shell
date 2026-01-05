d1f90cc05fb57c248f7e26f7be8ef26ca435802e
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-05 20:01:20 +0100

    parser.c

---
+++ parser.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+char **split_line(char *line)
+{
+	int bufsize = 64, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		perror("allocation error");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[i] = strdup(token);
+		i++;
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
+
+void free_array(char **arr)
+{
+	int i = 0;
+
+	if (!arr)
+		return;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
