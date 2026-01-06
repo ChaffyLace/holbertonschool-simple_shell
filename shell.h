013d307f169e874d4b2baca38f22e954357775a5
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-06 17:55:59 +0100

    Task 4

--- shell.h
+++ shell.h
@@ -2,18 +2,19 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 
 extern char **environ;
 
 char **split_line(char *line);
 void free_array(char **arr);
+char *_getenv(const char *name);
 char *find_path(char *cmd);
-void execute_command(char **args, char *line, char *name, int count);
+void execute_command(char **args, char *name, int count);
 
 #endif
