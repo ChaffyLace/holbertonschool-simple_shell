e6683884e8f643d907ed1c4eeae84bae3d507035
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-06 18:31:29 +0100

    Task 6

--- builtins.c
+++ builtins.c
@@ -8,11 +8,23 @@
  */
 int verifier_builtin(char **args, char *ligne)
 {
+	int i;
+
 	if (strcmp(args[0], "exit") == 0)
 	{
 		liberer_grille(args);
 		free(ligne);
 		exit(0);
+	}
+
+	if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i]; i++)
+		{
+			write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		return (1);
 	}
 	return (0);
 }
