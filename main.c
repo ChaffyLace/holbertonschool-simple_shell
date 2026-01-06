7276683fbae68ecd48274d82bce06d803da5f752
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-06 10:55:06 +0100

    Update task 3

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include "shell.h"
 
-/**
- * main - Main loop for the shell
- * @ac: Argument count
- * @av: Argument vector
- * Return: 0 on success
- */
 int main(int ac, char **av)
 {
 	char *line = NULL;
@@ -14,32 +8,32 @@ int main(int ac, char **av)
 	char **args;
 	int count = 0;
 
-	(void)ac; 
+	(void)ac;
+
 	while (1)
 	{
 		count++;
+
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
 
 		read = getline(&line, &len, stdin);
-		
 		if (read == -1)
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			free(line);
 			break;
 		}
+
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
 		args = split_line(line);
-		
-		if (args[0] != NULL)
-		{
+		if (args && args[0])
 			execute_command(args, line, av[0], count);
-		}
+
 		free_array(args);
 	}
+	free(line);
 	return (0);
 }
