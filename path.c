3352b6ff9545f4f65594d9041cf92a97d9e0ecb4
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-06 17:56:45 +0100

    Task 4

--- path.c
+++ path.c
@@ -1,43 +1,56 @@
 #include "shell.h"
 
 /**
- * find_path - finds full path of a command
+ * _getenv - finds an environment variable
+ * @name: variable name
+ * Return: value string
+ */
+char *_getenv(const char *name)
+{
+	int i, len;
+
+	len = strlen(name);
+	for (i = 0; environ[i]; i++)
+	{
+		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+			return (environ[i] + len + 1);
+	}
+	return (NULL);
+}
+
+/**
+ * find_path - finds the full path for a command
  * @cmd: command name
- *
  * Return: full path or NULL
  */
 char *find_path(char *cmd)
 {
-	char *path, *path_copy, *dir, *full_path;
+	char *path, *path_cp, *dir, *full;
 	struct stat st;
 
-	path = getenv("PATH");
-	if (!path)
+	path = _getenv("PATH");
+	if (!path || strlen(path) == 0)
 		return (NULL);
 
-	path_copy = strdup(path);
-	dir = strtok(path_copy, ":");
-
+	path_cp = strdup(path);
+	dir = strtok(path_cp, ":");
 	while (dir)
 	{
-		full_path = malloc(strlen(dir) + strlen(cmd) + 2);
-		if (!full_path)
-		{
-			free(path_copy);
+		full = malloc(strlen(dir) + strlen(cmd) + 2);
+		if (!full)
 			return (NULL);
-		}
-		sprintf(full_path, "%s/%s", dir, cmd);
+		strcpy(full, dir);
+		strcat(full, "/");
+		strcat(full, cmd);
 
-		if (stat(full_path, &st) == 0)
+		if (stat(full, &st) == 0)
 		{
-			free(path_copy);
-			return (full_path);
+			free(path_cp);
+			return (full);
 		}
-
-		free(full_path);
+		free(full);
 		dir = strtok(NULL, ":");
 	}
-
-	free(path_copy);
+	free(path_cp);
 	return (NULL);
 }
