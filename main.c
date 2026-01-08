181bb05163635d9229751a356c3fe1369ca8b6cd
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 22:48:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - simple shell loop
- * @ac: argument count
- * @av: argument vector
- * Return: 0
+ * @ac: arg count
+ * @av: arg vector
+ * Return: exit status
  */
 int main(int ac, char **av)
 {
@@ -13,16 +13,14 @@ int main(int ac, char **av)
 	ssize_t n_lus;
 	char **args;
 	int cpt = 0;
-	int last_status = 0;
+	int status = 0;
 
 	(void)ac;
-
 	while (1)
 	{
 		cpt++;
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
-
 		n_lus = getline(&ligne, &taille, stdin);
 		if (n_lus == -1)
 		{
@@ -30,27 +28,17 @@ int main(int ac, char **av)
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
 		if (ligne[n_lus - 1] == '\n')
 			ligne[n_lus - 1] = '\0';
-
 		args = decouper_ligne(ligne);
-		if (!args || !args[0])
+		if (args && args[0])
 		{
-			liberer_grille(args);
-			continue;
+			if (verifier_builtin(args, ligne))
+				continue;
+			status = executer(args, av[0], cpt);
 		}
-
-		if (verifier_builtin(args, ligne, last_status))
-		{
-			liberer_grille(args);
-			continue;
-		}
-
-		last_status = executer(args, av[0], cpt);
 		liberer_grille(args);
 	}
-
 	free(ligne);
-	return (0);
+	return (status);
 }
