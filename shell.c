554127784930683e2a6677163bd0ece4405f83b3
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 12:10:49 +0100

    shell.c

--- shell.c
+++ shell.c
@@ -1,32 +1,7 @@
 #include "shell.h"
 
 /**
- * split_line - split input line into arguments
- * @line: input line
- * Return: NULL-terminated array of strings
- */
-char **split_line(char *line)
-{
-	char **args;
-	char *token;
-	int i = 0;
-
-	args = malloc(sizeof(char *) * 64);
-	if (!args)
-		return (NULL);
-
-	token = strtok(line, " \n\t");
-	while (token)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " \n\t");
-	}
-	args[i] = NULL;
-	return (args);
-}
-
-/**
- * main - Simple shell 0.1 +
+ * main - Simple shell 0.1
  * Return: 0
  */
 int main(void)
@@ -35,7 +10,7 @@ int main(void)
 	size_t len = 0;
 	ssize_t read;
 	pid_t pid;
-	char **args;
+	char *argv[2];
 
 	while (1)
 	{
@@ -46,25 +21,25 @@ int main(void)
 		if (read == -1)
 			break;
 
-		if (read > 1)
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		if (*line == '\0')
+			continue;
+
+		argv[0] = line;
+		argv[1] = NULL;
+
+		pid = fork();
+		if (pid == 0)
 		{
-			args = split_line(line);
-			if (!args || !args[0])
-				continue;
-
-			pid = fork();
-			if (pid == 0)
-			{
-				execve(args[0], args, environ);
-				perror("./shell");
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				wait(NULL);
-			}
-
-			free(args);
+			execve(argv[0], argv, environ);
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
 		}
 	}
 
