4bae186b21fc5616590f54c1a0ea2eb5a842de12
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 15:24:37 +0100

    task

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * main - main shell loop
+ * main - simple shell loop
  * @ac: arg count
  * @av: arg vector
  * Return: 0
@@ -11,16 +11,15 @@ int main(int ac, char **av)
 	char *ligne = NULL;
 	size_t taille = 0;
 	ssize_t n_lus;
-	char **arguments;
-	int compteur = 0;
+	char **args;
+	int cpt = 0;
 	(void)ac;
 
 	while (1)
 	{
-		compteur++;
+		cpt++;
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
-
 		n_lus = getline(&ligne, &taille, stdin);
 		if (n_lus == -1)
 		{
@@ -28,21 +27,19 @@ int main(int ac, char **av)
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
 		if (ligne[n_lus - 1] == '\n')
 			ligne[n_lus - 1] = '\0';
-
-		arguments = decouper_ligne(ligne);
-		if (arguments && arguments[0])
+		args = decouper_ligne(ligne);
+		if (args && args[0])
 		{
-			if (verifier_builtin(arguments, ligne))
+			if (verifier_builtin(args, ligne))
 			{
-				liberer_grille(arguments);
+				liberer_grille(args);
 				continue;
 			}
-			executer(arguments, av[0], compteur);
+			executer(args, av[0], cpt);
 		}
-		liberer_grille(arguments);
+		liberer_grille(args);
 	}
 	free(ligne);
 	return (0);
