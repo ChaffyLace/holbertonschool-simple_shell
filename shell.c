98c6560c5868c6eade2647927db89295813fd3e9
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 12:08:50 +0100

    Update

--- shell.c
+++ shell.c
@@ -1,73 +1,73 @@
 #include "shell.h"
 
 /**
- * main - Point d'entrée du shell simple
- * Return: Toujours 0
+ * split_line - split input line into arguments
+ * @line: input line
+ * Return: NULL-terminated array of strings
+ */
+char **split_line(char *line)
+{
+	char **args;
+	char *token;
+	int i = 0;
+
+	args = malloc(sizeof(char *) * 64);
+	if (!args)
+		return (NULL);
+
+	token = strtok(line, " \n\t");
+	while (token)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \n\t");
+	}
+	args[i] = NULL;
+	return (args);
+}
+
+/**
+ * main - Simple shell 0.1 +
+ * Return: 0
  */
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t nread;
-	char **argv;
-	char *token;
-	int i;
-	int status;
-	char *cmd_path;
-	pid_t child_pid;
+	ssize_t read;
+	pid_t pid;
+	char **args;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(1, "$ ", 2);
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+			break;
+
+		if (read > 1)
 		{
-			free(line);
-			exit(0);
-		}
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-		argv = malloc(sizeof(char *) * 64);
-		if (!argv)
-			continue;
-		i = 0;
-		token = strtok(line, " ");
-		while (token != NULL)
-		{
-			argv[i++] = strdup(token);
-			token = strtok(NULL, " ");
-		}
-		argv[i] = NULL;
-		if (argv[0] == NULL)
-		{
-			free(argv);
-			continue;
-		}
-		if (handle_builtin(argv))
-			continue;
-		cmd_path = get_full_path(argv[0]);
-		if (cmd_path == NULL)
-		{
-			perror("./hsh");
-			free_args(argv);
-			continue;
-		}
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			if (execve(cmd_path, argv, environ) == -1)
+			args = split_line(line);
+			if (!args || !args[0])
+				continue;
+
+			pid = fork();
+			if (pid == 0)
 			{
-				perror("execve");
-				exit(1);
+				execve(args[0], args, environ);
+				perror("./shell");
+				exit(EXIT_FAILURE);
 			}
+			else
+			{
+				wait(NULL);
+			}
+
+			free(args);
 		}
-		else
-			wait(&status);
-		if (strcmp(cmd_path, argv[0]) != 0)
-			free(cmd_path);
-		free_args(argv);
 	}
+
 	free(line);
 	return (0);
 }
